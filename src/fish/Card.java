4415c098793e9d97ee3c2020e6b7118d9f2218ea
Iburinoc <iburinoc@gmail.com>
2015-05-27 13:06:54 -0400

    Added hashcode and equals to Card

--- src/fish/Card.java
+++ src/fish/Card.java
@@ -1,43 +1,68 @@
 package fish;
+
 /**
  * A class that represents a card that holds a suit and a value.
  */
 public class Card {
 
-    /**
-     * Value of card:
-     * 0: 2 or 9
-     * 1: 3 or T
-     * 2: 4 or J
-     * 3: 5 or Q
-     * 4: 6 or K
-     * 5: 7 or A
-     */
-    public final int value;
+	/**
+	 * Value of card:
+	 * 0: 2 or 9
+	 * 1: 3 or T
+	 * 2: 4 or J
+	 * 3: 5 or Q
+	 * 4: 6 or K
+	 * 5: 7 or A
+	 */
+	public final int value;
 
-    /**
-     * Value of suit from 0 to 7.
-     * 0: Low clubs
-     * 1: High clubs
-     * 2: Low diamonds
-     * 3: High diamonds
-     * 4: Low hearts
-     * 5: High hearts
-     * 6: Low spades
-     * 7: High spades
-     */
-    public final int suit;
+	/**
+	 * Value of suit from 0 to 7.
+	 * 0: Low clubs
+	 * 1: High clubs
+	 * 2: Low diamonds
+	 * 3: High diamonds
+	 * 4: Low hearts
+	 * 5: High hearts
+	 * 6: Low spades
+	 * 7: High spades
+	 */
+	public final int suit;
 
-    /**
-     * Constructor for Card class accepting value and suit
-     * @param value Value of card
-     * @param suit Suit of card
-     */
-    public Card(int value, int suit) {
-        if (value < 0 || value > 5 || suit < 0 || suit > 7) {
-            throw new IllegalArgumentException();
-        }
-        this.value = value;
-        this.suit = suit;
-    }
+	/**
+	 * Constructor for Card class accepting value and suit
+	 * 
+	 * @param value
+	 *                Value of card
+	 * @param suit
+	 *                Suit of card
+	 */
+	public Card(int value, int suit) {
+		if (value < 0 || value > 5 || suit < 0 || suit > 7) {
+			throw new IllegalArgumentException();
+		}
+		this.value = value;
+		this.suit = suit;
+	}
+
+	/**
+	 * returns a hashcode uniquely representing this card in the form suit *
+	 * 6 + value
+	 */
+	@Override
+	public int hashCode() {
+		return suit * 6 + value;
+	}
+
+	/**
+	 * determines if two card objects are equivalent
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof Card)) {
+			return false;
+		}
+		Card c = (Card) o;
+		return c.value == value && c.suit == suit;
+	}
 }
