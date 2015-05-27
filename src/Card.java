5b5ab09439eb4a034b850b6e228fa6aed2b5fac2
AndreyBorisKhesin <andrey.khesin@gmail.com>
2015-05-27 11:09:03 -0400

    some comments

---
+++ src/Card.java
@@ -0,0 +1,19 @@
+/**
+ * A class that represents a card that holds a suit and a value.
+ */
+public class Card {
+
+    /**
+     * Value of card from 0 to 5.
+     */
+    private int v;
+
+    /**
+     * Value of suit from 0 to 7.
+     * Odd numbers are high suits.
+     * Even numbers are low suits.
+     *
+     */
+    private int s;
+
+}
