6f9e5eabeed253fdd1f4d51a25ecb8aa410af197
AndreyBorisKhesin <andrey.khesin@gmail.com>
2015-06-11 11:55:57 -0400

    fixed 1's problem

--- src/fish/Matrix.java
+++ src/fish/Matrix.java
@@ -1,5 +1,7 @@
 package fish;
 
+import java.util.Arrays;
+
 import static fish.Util.isZero;
 
 /**
@@ -9,39 +11,46 @@ public final class Matrix {
 	/**
 	 * The array of elements of the Matrix.
 	 */
-	public final double[][] _;
+	public final double[][] m;
 
 	/**
 	 * Constructor for Matrix using a two-dimensional array.
 	 *
-	 * @param _ The array of the Matrix.
+	 * @param m The array of the Matrix.
 	 */
-	public Matrix(double[][] _) {
-		this._ = _;
+	public Matrix(double[][] m) {
+		this.m = m;
 	}
 
 	/**
 	 * Constructs a modified Matrix based on a vector.
 	 *
-	 * @param vector The input vector to custruct a Matrix off of.
+	 * @param vector The input vector to construct a Matrix off of.
 	 * @param units The values around which to invert.
 	 * @return The resultant Matrix.
 	 */
-	public static Matrix diagonalReciprocal(double[] vector, int[] units) {
+	public Matrix diagonalReciprocal(double[] vector, int[] units) {
 		if (vector.length != units.length) {
 			throw new IllegalArgumentException
 					("Input dimensions do not match.");
 		}
 		double[][] result = new double[vector.length][vector.length];
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[0].length; j++) {
+				if (isZero(m[i][j] - 1)) {
+					m[i][j] = vector[j] / units[j];
+				}
+			}
+		}
 		for (int i = 0; i < result.length; i++) {
 			if (!isZero(vector[i])) {
 				result[i][i] = units[i] / vector[i];
 			}
 		}
-		return new Matrix(result);
+		return this.x(new Matrix(result));
 	}
 
-	public static Matrix diagonalReciprocal(double[] vector) {
+	public Matrix diagonalReciprocal(double[] vector) {
 		int[] units = new int[vector.length];
 		for (int i = 0; i < units.length; i++) {
 			units[i] = 1;
@@ -50,23 +59,18 @@ public final class Matrix {
 	}
 
 	public double[] sum() {
-		double[][] sum = new double[_.length][1];
+		double[] sum = new double[m.length];
 		for (int i = 0; i < sum.length; i++) {
-			sum[i][0] = 1;
+			sum[i] = Arrays.stream(m[i]).filter(x -> !isZero(x - 1)).sum();
 		}
-		Matrix result = this.x(new Matrix(sum));
-		double[] vector = new double[result._.length];
-		for (int i = 0; i < result._.length; i++) {
-			vector[i] = result._[i][0];
-		}
-		return vector;
+		return sum;
 	}
 
 	public Matrix t() {
-		double[][] result = new double[_[0].length][_.length];
-		for (int i = 0; i < _.length; i++) {
-			for (int j = 0; j < _[0].length; j++) {
-				result[j][i] = _[i][j];
+		double[][] result = new double[m[0].length][m.length];
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[0].length; j++) {
+				result[j][i] = m[i][j];
 			}
 		}
 		return new Matrix(result);
@@ -79,16 +83,16 @@ public final class Matrix {
 	 * @return The product.
 	 */
 	public Matrix x(Matrix matrix) {
-		if (_[0].length != matrix._.length) {
+		if (m[0].length != matrix.m.length) {
 			throw new IllegalArgumentException
 					("Matrix dimensions do not match.");
 		}
-		double[][] product = new double[_.length][matrix._[0].length];
+		double[][] product = new double[m.length][matrix.m[0].length];
 		for (int i = 0; i < product.length; i++) {
 			for (int j = 0; j < product[0].length; j++) {
-				for (int k = 0; k < _[0].length; k++) {
+				for (int k = 0; k < m[0].length; k++) {
 					product[i][j] +=
-							_[i][k] * _[k][j];
+							m[i][k] * m[k][j];
 				}
 			}
 		}
