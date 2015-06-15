ecafc92e3503a2bbafe80655e48bb08b20685b46
AndreyBorisKhesin <andrey.khesin@gmail.com>
2015-06-15 18:41:53 -0400

    fixed my own screen

--- src/fish/client/ui/Resolution.java
+++ src/fish/client/ui/Resolution.java
@@ -3,7 +3,7 @@ package fish.client.ui;
 import java.awt.Dimension;
 
 public enum Resolution {
-	_1280_720(1280, 720), _1980_1080(1980, 1080), _1024_576(1024, 675), _ANDREYS_SCREEN(1248, 702);
+	_1280_720(1280, 720), _1980_1080(1980, 1080), _1024_576(1024, 675), _ANDREYS_SCREEN(1232, 693);
 
 	public final Dimension d;
 	public final String name;
